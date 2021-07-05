de9d8453260e95a7d1277b09628ffa27a562ff3c
Kertich <kipsangkertich@gmail.com>
2021-07-05 13:22:23 +0300

    update header

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,26 +1,70 @@
 import React from 'react';
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+//import { Avatar } from '@material-ui/core';
+import {Avatar,  IconButton } from '@material-ui/core';
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
-            <img alt="File:Facebook Logo.png" src="//upload.wikimedia.org/wikipedia/commons/4/44/Facebook_Logo.png" 
-            decoding="async" 
-            width="512" 
-            height="513" 
-            data-file-width="512" 
-            data-file-height="513"/>
-            <div className="header__input">
-                <SearchIcon />
-                <input type="text" />
-            </div>
+                <img alt="File:Facebook Logo.png" src="//upload.wikimedia.org/wikipedia/commons/4/44/Facebook_Logo.png" 
+                decoding="async" 
+                width="512" 
+                height="513" 
+                data-file-width="512" 
+                data-file-height="513"/>
+                <div className="header__input">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
             </div>
 
-            <div className= "header__middle"></div>
+            <div className= "header__center">
+                <div className="header__option">
+                    <HomeIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SubscriptionOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large"/>
+                </div>
+            </div>
 
-            <div className="header__right"></div>
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Hackertich</h4>
+                </div>
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+            </div>
         </div>
     )
 }
