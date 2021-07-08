364722ee133f6a5e8c59ae3a199f478f707ba790
Kertich <kipsangkertich@gmail.com>
2021-07-08 15:57:26 +0300

    add material-ui icons

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
 import './Feed.css';
 function Feed() {
     return (
         <div className="feed">
         <StoryReel />
-        {/* StoryReel */}
+        <MessageSender />
          {/*MessageSender */}   
         </div>
     )
