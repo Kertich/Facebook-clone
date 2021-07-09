86f3bb49a09e9f66f88685d5226c0e2a56935866
Kertich <kipsangkertich@gmail.com>
2021-07-09 13:31:54 +0300

    style feed

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -25,7 +25,14 @@ function Feed() {
              image = "https://miro.medium.com/fit/c/262/262/2*7kB23qvO6eQ_ZQE4jM3OQg.jpeg" alt="kipsang kertich – Medium" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 262px; height: 262px; margin: 0px;"
         
          />
-         <Post />
+          <Post 
+             profilePic="https://pbs.twimg.com/profile_images/1149589217573425153/XfMudxUX_400x400.jpg" alt="Obed Kertich (@Kipsang_Kertich) | Twitter" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 383px; margin: 0px;"
+             message = "React is awesome!!!"
+             timestamp="This is the timestamp"
+             username='@kipsangkertich'
+             image = "https://miro.medium.com/fit/c/262/262/2*7kB23qvO6eQ_ZQE4jM3OQg.jpeg" alt="kipsang kertich – Medium" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 262px; height: 262px; margin: 0px;"
+        
+         />
         </div>
     )
 }
--- fb-clone/src/Post.js
+++ fb-clone/src/Post.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Post.css';
 import { Avatar } from "@material-ui/core";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutLineIcon from "@material-ui/icons/ChatBubbleOutline";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import { ExpandMoreOutlined } from "@material-ui/icons";
+
 
 function Post({profilePic, image, username, timestamp, message }) {
     return (
@@ -15,8 +20,16 @@ function Post({profilePic, image, username, timestamp, message }) {
             </div>
             <div className="post__bottom">
                 <p>{message}</p>
-                <div className="post__image">
+            </div>
+
+            <div className="post__image">
                     <img src={image} alt="" />
+            </div>
+
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUpIcon />
+                    <p>Like</p>
                 </div>
             </div>
         </div>
