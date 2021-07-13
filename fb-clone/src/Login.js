d329a05d6c1c916459b12f5b43c20fd10c4e3820
Kertich <kipsangkertich@gmail.com>
2021-07-13 15:43:14 +0300

    data layer

--- fb-clone/src/Login.js
+++ fb-clone/src/Login.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import './Login.css';
 import { auth, provider } from "./firebase";
+import { actionTypes } from "./reducer";
+import { useStateValue } from "./StateProvider";
 
 
 function Login() {
+    const [state, dispatch] = useStateValue();
+
     const signIn = () => {
         // sign in..
         auth.signInWithPopup(provider)
         .then(result => {
-            console.log(result.user)
+
+
+            dispatch({
+                    type: actionTypes.SET_USER,
+                    user:result.user,
+            })
         }).catch(error => alert(error.message))
     };
     return (
