ba0851abbd38e5f2809cdddca8cf2b411495d6fe
Kertich <kipsangkertich@gmail.com>
2021-07-14 15:01:38 +0300

    fix prop error

--- fb-clone/src/StoryReel.js
+++ fb-clone/src/StoryReel.js
@@ -6,33 +6,33 @@ function StoryReel() {
     return (
         <div className="storyReel">
             <Story 
-                image="https://static.vecteezy.com/system/resources/previews/001/444/856/non_2x/plain-background-with-happy-girl-with-a-wooden-stick-free-vector.jpg" alt="Plain background with happy girl with a wooden stick 1444856 Vector Art at  Vecteezy" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 251.232px; margin: 0px;"
-                profileSrc="https://avatars.githubusercontent.com/u/38984721?s=400&amp;u=d474912b1ba7fcbbffe3a89201c6f30dd291e8e8&amp;v=4" width="200" height="200" alt="@Kertich"
+                image="https://static.vecteezy.com/system/resources/previews/001/444/856/non_2x/plain-background-with-happy-girl-with-a-wooden-stick-free-vector.jpg" 
+                profileSrc="https://avatars.githubusercontent.com/u/38984721?s=400&amp;u=d474912b1ba7fcbbffe3a89201c6f30dd291e8e8&amp;v=4" 
                 title="Hackertich"
             />
             <Story 
-                image="https://cdn.wallpapersafari.com/49/66/nXGWoi.jpg" alt="21+] Background Plain on WallpaperSafari" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 258.188px; height: 459px; margin: 0px;"
-                profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg" alt="Elon Musk is &amp;quot;the biggest architect of our time&amp;quot; says Jan Boelen" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 383px; margin: 0px;"
+                image="https://cdn.wallpapersafari.com/49/66/nXGWoi.jpg" 
+                profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg" 
                 title="Elon Musk"
             />
             <Story 
-                image="https://c8.alamy.com/comp/2ARJ1JM/tropical-flowers-seamless-plain-background-with-white-and-pink-frangipani-flower-floral-texture-for-wallpaper-or-backdrop-spring-floral-concept-2ARJ1JM.jpg" alt="tropical flowers seamless plain background with white and pink frangipani  flower, floral texture for wallpaper or backdrop. spring floral concept  Stock Photo - Alamy" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 281.652px; margin: 0px;"
-                profileSrc="https://images.newscientist.com/wp-content/uploads/2019/06/18142824/einstein.jpg" alt="Albert Einstein | World-famous theoretical physicist | New Scientist" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 215.438px; margin: 0px;"
+                image="https://c8.alamy.com/comp/2ARJ1JM/tropical-flowers-seamless-plain-background-with-white-and-pink-frangipani-flower-floral-texture-for-wallpaper-or-backdrop-spring-floral-concept-2ARJ1JM.jpg" 
+                profileSrc="https://images.newscientist.com/wp-content/uploads/2019/06/18142824/einstein.jpg"  
                 title="Albert Einstein"
             />
             <Story 
-                image="https://static.vecteezy.com/system/resources/previews/001/444/856/non_2x/plain-background-with-happy-girl-with-a-wooden-stick-free-vector.jpg" alt="Plain background with happy girl with a wooden stick 1444856 Vector Art at  Vecteezy" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 251.232px; margin: 0px;"
-                profileSrc="https://avatars.githubusercontent.com/u/38984721?s=400&amp;u=d474912b1ba7fcbbffe3a89201c6f30dd291e8e8&amp;v=4" width="200" height="200" alt="@Kertich"
+                image="https://static.vecteezy.com/system/resources/previews/001/444/856/non_2x/plain-background-with-happy-girl-with-a-wooden-stick-free-vector.jpg" 
+                profileSrc="https://avatars.githubusercontent.com/u/38984721?s=400&amp;u=d474912b1ba7fcbbffe3a89201c6f30dd291e8e8&amp;v=4"
                 title="Hackertich"
             />
             <Story 
-                image="https://cdn.wallpapersafari.com/49/66/nXGWoi.jpg" alt="21+] Background Plain on WallpaperSafari" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 258.188px; height: 459px; margin: 0px;"
-                profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg" alt="Elon Musk is &amp;quot;the biggest architect of our time&amp;quot; says Jan Boelen" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 383px; margin: 0px;"
+                image="https://cdn.wallpapersafari.com/49/66/nXGWoi.jpg" 
+                profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg" 
                 title="Elon Musk"
             />
             <Story 
-                image="https://c8.alamy.com/comp/2ARJ1JM/tropical-flowers-seamless-plain-background-with-white-and-pink-frangipani-flower-floral-texture-for-wallpaper-or-backdrop-spring-floral-concept-2ARJ1JM.jpg" alt="tropical flowers seamless plain background with white and pink frangipani  flower, floral texture for wallpaper or backdrop. spring floral concept  Stock Photo - Alamy" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 281.652px; margin: 0px;"
-                profileSrc="https://images.newscientist.com/wp-content/uploads/2019/06/18142824/einstein.jpg" alt="Albert Einstein | World-famous theoretical physicist | New Scientist" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 215.438px; margin: 0px;"
+                image="https://c8.alamy.com/comp/2ARJ1JM/tropical-flowers-seamless-plain-background-with-white-and-pink-frangipani-flower-floral-texture-for-wallpaper-or-backdrop-spring-floral-concept-2ARJ1JM.jpg" 
+                profileSrc="https://images.newscientist.com/wp-content/uploads/2019/06/18142824/einstein.jpg" alt="Albert Einstein | World-famous theoretical physicist | New Scientist" 
                 title="Albert Einstein"
             />
             {/*Story*/}
--- fb-clone/src/Widgets.js
+++ fb-clone/src/Widgets.js
@@ -4,8 +4,9 @@ import './Widgets.css';
 function Widgets() {
     return (
         <div className="widgets">
-            <iframe
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBead-work-and-watch-brand-1857728801171577%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+            <iframe 
+                title="Bead work page"
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBead-work-and-watch-brand-1857728801171577%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
                 width="340"
                 height="100%"
                 style={{ border: "none", overflow:
